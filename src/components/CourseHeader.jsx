13a1b1031dfa7b9168ddb9f8d548e6d9493bf0a0
trancongthuan <trancongthuan353@gmail.com>
2026-09-14 20:25:08 -0700

    slot 6 - custom hook

--- src/components/CourseHeader.jsx
+++
@@ -1,9 +0,0 @@
-function CourseHeader() {
-    return (
-        <>
-            <h1>SBA302 - Integrate single page application with Spring Boot(SBA301)</h1>
-        </>
-    )
-}
-
-export default CourseHeader;
--- src/components/Hello.jsx
+++
@@ -1,9 +0,0 @@
-function Hello() {
-    return (
-        <>
-            <h1>I want to say hello to you!</h1>
-        </>
-    )
-}
-
-export default Hello;
